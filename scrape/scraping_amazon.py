990f5aeb52c8c9bede4a2d8be31f5a1be9ba9cf3
Ana Alves <anadomingues87@gmail.com>
2016-11-26 16:50:50 -0800

    comment code

--- scrape/scraping_amazon.py
+++ scrape/scraping_amazon.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 import feedparser
-from selenium import webdriver    #open webdriver for specific browser
+from selenium import webdriver    # open webdriver for specific browser
 from selenium.webdriver.common.keys import Keys   # for necessary browser action
 from selenium.webdriver.common.by import By    # For selecting html code
 import time
@@ -23,11 +23,12 @@ def amazon_scrp(interval, url):
     driver = webdriver.Chrome()
     driver.get(url)
     time.sleep(9)
-    for i in xrange(0,interval):
+    for i in xrange(0, interval):
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
         time.sleep(10)
-        for page in xrange(0+(i*2),(i*2)+2):
-            for im in xrange(1,25):
+        for page in xrange(0+(i*2), (i*2)+2):
+            # number of products per scroll
+            for im in xrange(1, 25):
                 products = driver.find_element_by_xpath('//*[@id="infinite-scroll-page-' + str(page) + '"]/div[' + str(im) + ']/div[2]/a')
                 product_description = products.get_attribute('title')
                 product_amazon_link = products.get_attribute('href')
@@ -36,13 +37,13 @@ def amazon_scrp(interval, url):
                 image_id = image_url.split('/')[5].split('.')[0]
                 price = driver.find_element_by_xpath('//*[@id="infinite-scroll-page-' + str(page) + '"]/div[' + str(im) + ']/div[3]/span')
                 product_price = price.text
-    #product_id.append(image_id)
+    # product_id.append(image_id)
                 entry = (image_id, product_description, product_price[1:], image_url, product_amazon_link)
                 database.append(entry)
                 print len(database)
-    #save figure
+    # save figure
                 resource = urllib.urlopen(image_url)
-                output = open("./images/" + image_id + ".jpg","wb")
+                output = open("./images/" + image_id + ".jpg", "wb")
                 output.write(resource.read())
                 output.close()
                 print "saved image ..."
@@ -55,10 +56,10 @@ if __name__ == '__main__':
     url_woman = 'https://www.amazon.com/gp/gift-finder?ageGroup=woman'
     url_man = 'https://www.amazon.com/gp/gift-finder?ageGroup=man'
     url_teen = 'https://www.amazon.com/gp/gift-finder?ageGroup=teen'
-    database1 = amazon_scrp(129,url_woman)
-    database2 = amazon_scrp(110,url_man)
-    database3 = amazon_scrp(77,url_teen)
+    database1 = amazon_scrp(129, url_woman)
+    database2 = amazon_scrp(110, url_man)
+    database3 = amazon_scrp(77, url_teen)
     total_database = database1 + database2 + database3
-    df = pd.DataFrame(total_database, columns=["image_id", "product_description", "product_price", "image_url","product_amazon_link"])
+    df = pd.DataFrame(total_database, columns=["image_id", "product_description", "product_price", "image_url", "product_amazon_link"])
     df = df.drop_duplicates(subset='image_id')
     df.to_csv('amazon_products_description.csv', sep='\t', encoding='utf-8')
